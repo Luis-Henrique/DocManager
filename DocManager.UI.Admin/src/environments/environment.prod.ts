0d0c15770ff1c2284fa039421051d5cf4b43f97e
Luis Henrique Gomes Cardoso <henriqueluis3104@hotmail.com>
2023-04-23 22:32:06 -0300

    Alteração DB

--- DocManager.UI.Admin/src/environments/environment.prod.ts
+++ DocManager.UI.Admin/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
-  url_reload: 'http://localhost:4200/#/login',
-  url_login: 'http://localhost:5000/api/v1/account',
-  url_api: 'http://localhost:5000/api/v1/',
+  url_reload: 'http://docmanager-api.azurewebsites.net/#/login',
+  url_login: 'http://docmanager-api.azurewebsites.net/api/v1/account',
+  url_api: 'http://docmanager-api.azurewebsites.net/api/v1/',
   /*
   url_reload: 'http://localhost:4200/#/login',
   url_login: 'https://backend-sysmanager.azurewebsites.net/api/v1/account',
