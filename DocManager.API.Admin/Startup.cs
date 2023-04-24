3cae850c28e159bf3c794c32043b2071d48bffd6
Luis Henrique <101938473+Luis-Henrique@users.noreply.github.com>
2023-04-24 19:38:27 -0300

    Update Startup.cs

--- DocManager.API.Admin/Startup.cs
+++ DocManager.API.Admin/Startup.cs
@@ -35,7 +35,7 @@ namespace DocManager.API.Admin
                 options.AddPolicy(name: CorsPolicy,
                                   builder =>
                                   {
-                                        builder.WithOrigins("https://front-doc-manager-deploy.vercel.app").WithMethods("GET", "OPTIONS", "PATCH", "DELETE", "POST", "PUT").AllowAnyHeader();
+                                        builder.WithOrigins("https://agreeable-pebble-079856410.3.azurestaticapps.net").WithMethods("GET", "OPTIONS", "PATCH", "DELETE", "POST", "PUT").AllowAnyHeader();
                                   });
             });
 
